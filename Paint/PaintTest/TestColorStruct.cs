cb24f75f08222487c33a5ef2d37f0de9e83a2fb7
serhii-z <sergey59070@gmail.com>
2020-01-11 17:10:02 +0200

    Add Test for constructor class Color

--- Paint/PaintTest/TestColorStruct.cs
+++ Paint/PaintTest/TestColorStruct.cs
@@ -5,7 +5,25 @@ using NUnit.Framework;
 
 namespace PaintTest
 {
+    [TestFixture]
     class TestColorStruct
     {
+        [Test]
+        public void TestParametersConstructor()
+        {
+            byte expectedRed = 10;
+            byte expectedGreen = 20;
+            byte expectedBlue = 30;
+
+            Paint.Draw.Color color = new Paint.Draw.Color(10, 20, 30);
+
+            byte actualRed = color.Red;
+            byte actualGreen = color.Green;
+            byte actualBlue = color.Blue;
+
+            Assert.AreEqual(expectedRed, actualRed);
+            Assert.AreEqual(expectedGreen, actualGreen);
+            Assert.AreEqual(expectedBlue, actualBlue);
+        }
     }
 }
