25c040728d8a46c24495a967471493d5581497f9
valerio810 <valeronmaestr@gmail.com>
2020-01-11 14:09:32 +0200

    Create File Keeper Droid

---
+++ Paint/Droid/Keeper/DrawKeeperFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Paint.Keeper;
+
+namespace Paint.Droid.Keeper
+{
+    public class DrawKeeperFactory : IDrawKeeperFactory
+    {
+        
+        public IDrawKeeper Create(EDrawKeeperType type)
+        {
+            switch (type)
+            {
+                case EDrawKeeperType.Internal:
+                    return new InternalKeeper();
+                case EDrawKeeperType.File:
+                    return new FileKeeperDroid();
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+        }
+    }
+}
---
+++ Paint/Droid/Keeper/FileKeeperDroid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Paint.Draw;
+using Paint.Keeper;
+
+namespace Paint.Droid.Keeper
+{
+    public class FileKeeperDroid : IDrawKeeper
+    {
+        public DrawModel Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(DrawModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
